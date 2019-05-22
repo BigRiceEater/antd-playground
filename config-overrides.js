0d13e708c5f3444fae5698b75ccb60ef6d2dda4d
BigRiceEater <34292181+BigRiceEater@users.noreply.github.com>
2019-05-22 10:23:00 +0800

    Configure theming ant design

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,16 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css'
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      '@card-shadow': '10px 10px 21px 0px rgba(0,0,0,1.0);',
+      '@primary-color': '#66CDAA'
+    }
   })
 );
