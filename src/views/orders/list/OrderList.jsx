c7082c987097a69f4e5cf9693825a51faf241fa3
BigRiceEater <34292181+BigRiceEater@users.noreply.github.com>
2019-05-23 12:12:18 +0800

    Display order status in list view

--- src/views/orders/list/OrderList.jsx
+++ src/views/orders/list/OrderList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { List } from 'antd';
+import { List, Row, Col } from 'antd';
 import DataMaker from './../../../util/data-maker';
 
 class OrderList extends Component {
@@ -18,7 +18,12 @@ class OrderList extends Component {
           dataSource={this.state.orders}
           renderItem={item => (
             <List.Item>
-              <Link to={`${match.url}/${item.id}`}> {item.id}</Link>
+              <Row type='flex' justify='space-between'>
+                <Col span={8}>
+                  <Link to={`${match.url}/${item.id}`}> {item.id}</Link>
+                </Col>
+                <Col span={4}>{item.status}</Col>
+              </Row>
             </List.Item>
           )}
         />
